62647b0904266ff585eaabc68129aadbd8b345ce
Spectre135 <matej.batistic@gmail.com>
2017-11-14 15:29:01 +0100

    add WCF Auth

---
+++ WcfAuth/WcfAuth/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WebCRU.Auth;
+
+namespace WcfAuth
+{
+
+    public class AuthService : IAuthService
+    {
+
+        public DAuth GetSession(string username)
+        {
+            return AuthWorker.CreateSession(username);
+        }
+        
+    }
+}
---
+++ WcfAuth/WcfAuth/DAO/DAOService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfAuth.Models;
+
+namespace WcfAuth.DAO
+{
+    class DAOService
+    {
+        public static void SaveNewSession(AuthSession _AuthSession)
+        {
+
+            using (Entities db = new Entities())
+            {
+                using (var tran = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.AuthSession.Add(_AuthSession);
+
+                        db.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        throw ex.InnerException;
+                    }
+                }
+
+            }
+        }
+
+        public static AuthSession GetSession(string SessionToken)
+        {
+            AuthSession response = new AuthSession();
+
+            using (Entities db = new Entities())
+            {
+                response = db.AuthSession.Where(b => b.SessionToken == SessionToken).FirstOrDefault();
+            }
+
+            return response;
+        }
+
+        public static Uporabniki GetUporabnik(string UserName)
+        {
+            Uporabniki response = new Uporabniki();
+
+            using (Entities db = new Entities())
+            {
+                response = db.Uporabnikis.Where(b => b.UporabnikID == UserName).FirstOrDefault();
+            }
+
+            return response;
+        }
+    }
+}
